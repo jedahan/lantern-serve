08421a0081e6f3bfbd045ad88faa091b347f1cd8
West Lane <west@paperequator.com>
2018-09-08 18:56:10 -0400

    Populate Database from Remote Peer
    - leverage cloudant-hosted database peers
    - user can customize to bootstrap data from anywhere
    - this allows each region to keep distinct data
    - lanterns could be configured to start from a different database entirely for new use cases (non-disaster related)
    - get rid of pre-populated database files stored directly inside git
    - we note however that SQLite files may still be loaded directly if desired as a starting point (and still synchronized as needed)
    
    
    Former-commit-id: 9859435ac990978103adcc4506886c60e5026934

--- util.js
+++ util.js
@@ -193,10 +193,23 @@ self.saveDeviceLocation = function(geo) {
 */
 self.CoreDatabase = self.PouchDB(self.getHttpAddress() + "/db/lnt");
 
+
+/**
+* Reference database to replicate from for network / community aggregated data
+*/
+self.CoreDatabasePeer = self.PouchDB(process.env.CORE_DB_PEER || "https://37bd9e99-2780-4965-8da8-b6b1ebb682bc-bluemix.cloudant.com/lantern-us-demo");
+
+
 /**
 * Map specific database for tiles
 */
 self.MapDatabase = self.PouchDB(self.getHttpAddress() + "/db/map");
+
+
+/**
+* Reference database to replicate from for maps
+*/
+self.MapDatabasePeer = self.PouchDB(process.env.MAP_DB_PEER || "https://37bd9e99-2780-4965-8da8-b6b1ebb682bc-bluemix.cloudant.com/lantern-us-maps");
 
 
 //----------------------------------------------------------------------  
