99a682726830d936ed9c4fcbf2a49cf09f575bb1
West Lane <west@paperequator.com>
2018-09-08 18:05:21 -0400

    Additional Rewrite Routes for PouchDB

--- middleware/rewrite.js
+++ middleware/rewrite.js
@@ -10,7 +10,7 @@ module.exports = function RewriteMiddleware(req,res,next) {
 
     // PouchDB web admin may need access to some or all of these at the root
     // https://github.com/pouchdb/express-pouchdb/issues/116
-    var paths = ['/_session', '/_all_dbs', '/_replicator', "/_uuids", 
+    var paths = ['/_session', '/_replicate', '/_membership', '/_node', '/_all_dbs', '/_replicator', "/_uuids", 
         '/_users', '/_utils', "/_active_tasks", "/lnt", "/map", "/verifytestdb"];
 
     // if we're requesting one of these, make sure to prefix with "/db"
