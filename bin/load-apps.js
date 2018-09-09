31a11921ba1b34707d209aee5b465b6ca033d279
West Lane <west@paperequator.com>
2018-09-09 18:20:48 -0400

    start-fresh command will auto-load database from remote source
    - this allows our image builders to automatically download internet data, without booting up the entire server
    
    
    Former-commit-id: ec284318becf3be3dff50df9de580d673ddcdd5d

--- bin/load-apps.js
+++ bin/load-apps.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
 /**
-* Lantern Web Updater
+* Lantern Web App Loader
 *
 * Makes it easy to keep our server up-to-date with latest static web assets
 *
 **/
 
+var path = require("path");
+
+var log = require("simple-node-logger").createSimpleLogger({
+    logFilePath: path.resolve(__dirname, "..", 'logs', 'app-loader.log'),
+    dateFormat:'YYYY.MM.DD'
+});
+
+
 var fs = require("fs-extra");
 var path = require("path");
 var request = require("request");
 var AdmZip = require("adm-zip");
-
-var util = require("../util");
-var log = util.Logger;
 
 var uri = "https://github.com/lantern-works/lantern-web/raw/master/build/latest.zip";
 var public_dir = path.resolve(__dirname, "../public");
@@ -22,18 +27,18 @@ var zip_file = path.resolve(__dirname, "../web.zip");
 var self = function(done) {
     fs.ensureDirSync(public_dir);
     fs.emptyDirSync(public_dir);
-    log.debug("[ui] directory: " + public_dir);
-    log.debug("[ui] download: " + uri);
+    log.info("[ui] directory: " + public_dir);
+    log.info("[ui] download: " + uri);
     request(uri)
         .on('error', function() {
            log.error("[ui] no access to update web assets");
         })
         .pipe(fs.createWriteStream(zip_file))
         .on('close', function () {
-            log.debug("[ui] archive downloaded");
+            log.info("[ui] archive downloaded");
             var zip = new AdmZip(zip_file); 
             zip.extractAllTo(public_dir);
-            log.debug("[ui] unzipped archive to public directory");
+            log.info("[ui] unzipped archive to public directory");
             fs.removeSync(zip_file);
             if (typeof(done) == "function") {
                 done();
