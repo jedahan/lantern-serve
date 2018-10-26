097abac47cf5849845f557815226a5d85fd1d58c
West Lane <west@paperequator.com>
2018-10-26 12:08:39 +0100

    WEB_ZIP Variable
    - allow custom location for the web application zipfile
    - this enables servers to point ot custom web code or forked versions

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,15 +4,11 @@ var log = util.Logger;
 
 module.exports = function CORSMiddleware(req, res, next) {
   
-    process.env.ORIGINS = process.env.ORIGINS +",lantern.global,lantern.local,localhost,localhost:3000,lantern-nexus-web-intelligent-chipmunk.mybluemix.net";
-    var allowed_origins = process.env.ORIGINS.split(",");
     try {
         if (req.headers.origin) {  
             var origin = req.headers.origin.split("://")[1];
-            if(allowed_origins.indexOf(origin) > -1){
-                var protocol = (req.secure ? "https://" : "http://");
-                res.setHeader('Access-Control-Allow-Origin', protocol+origin);
-            }
+            var protocol = (req.secure ? "https://" : "http://");
+            res.setHeader('Access-Control-Allow-Origin', protocol+origin);
         }
     }
     catch(e) {
