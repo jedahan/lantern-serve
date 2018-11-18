61a08b0b74d3dcf819ff827b3bda521345dadf91
West Lane <west@paperequator.com>
2018-11-18 13:57:18 -0500

    API Route Format Fixes

--- web/routes/api.js
+++ web/routes/api.js
@@ -14,30 +14,52 @@ const db = util.CoreDatabase;
 * Allows user to easily load latest web assets onto the server
 */
 module.exports = (serv) => {
-  
-    serv.get("/api/version", (req, res) => {
-        var package_file_path = path.join(__dirname, "..", "package.json");
-        var obj = JSON.parse(fs.readFileSync(package_file_path, 'utf8'));
-        res.status(200).json({"name":"Lantern (JavaScript)","version": obj.version});
+
+
+
+    //------------------------------------------------------------------
+    serv.get("/api/id", (req, res) => {
+        res.status(200).send({"id": util.getDeviceIdentifier()});
+    });
+
+    serv.get("/api/name", (req, res) => {
+        var id = util.getDeviceIdentifier();
+        util.getDeviceName().then((name) => {
+            res.status(200).send({
+                "id": id, 
+                "name": name
+            });
+        });
     });
 
     serv.post("/api/name", bodyParser.json(), (req, res) => {
         var id = util.getDeviceIdentifier();
         if (req.body.name && typeof(req.body.name) == "string") {
             if (req.body.name.length != 3) {
-                return res.status(409).json({"ok": false, "message": "Name must be 3 characters in length"});
+                return res.status(409).json(
+                        {"ok": false, "message": "Name must be 3 characters in length"}
+                    );
             }
             log.info("setting name of host to: " + req.body.name);
             
             util.saveDeviceName(req.body.name);
 
-            return res.status(201).json({"ok": true, "id": id, "name": req.body.name});
+            return res.status(201).json(
+                    {"ok": true, "id": id, "name": req.body.name}
+                );
         }
         else {
-            return res.status(409).json({"ok": false, "id": id, "message": "Required parameter not found: name"});
+            return res.status(409).json(
+                    {"ok": false, "id": id, "message": "Required parameter not found: name"}
+                );
         }
-    });
+    });  
 
+    serv.get("/api/version", (req, res) => {
+        var package_file_path = path.join(__dirname, "..", "package.json");
+        var obj = JSON.parse(fs.readFileSync(package_file_path, 'utf8'));
+        res.status(200).json({"name":"Lantern (JavaScript)","version": obj.version});
+    });
 
     serv.get("/api/info", (req, res) => {
         var id = util.getDeviceIdentifier();
@@ -51,17 +73,8 @@ module.exports = (serv) => {
     });
 
 
-    serv.get("/api/name", (req, res) => {
-        var id = util.getDeviceIdentifier();
-        util.getDeviceName().then((name) => {
-            res.status(200).send({
-                "id": id, 
-                "name": name
-            });
-        });
-    });
 
-
+    //------------------------------------------------------------------
     serv.get("/api/geo", (req, res) => {
         var id = util.getDeviceIdentifier();
         db.get("d:"+ id)
@@ -85,20 +98,21 @@ module.exports = (serv) => {
         if (req.body.geo && typeof(req.body.geo) == "string") {
             util.saveDeviceLocation(req.body)
                 .then(() => {
-                    res.status(201).send({"ok": true, "id": id, "geo": req.body.geo});
+                    res.status(201).send(
+                            {"ok": true, "id": id, "geo": req.body.geo}
+                        );
                 });
         }        
         else {
-            return res.status(409).json({"ok": false, "id": id, "message": "Required parameter not found: geo"});
+            return res.status(409).json(
+                {"ok": false, "id": id, "message": "Required parameter not found: geo"}
+                );
         }
     });
 
 
-    serv.get("/api/id", (req, res) => {
-        res.status(200).send({"id": util.getDeviceIdentifier()});
-    });
 
-
+    //------------------------------------------------------------------
     serv.post("/api/ui", (req, res) => {
         require("../bin/load-apps")(() => {
             res.status(201).json({"ok": true});
