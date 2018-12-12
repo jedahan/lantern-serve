c8660be529df7f5122550d6092d6440401a05f40
West Lane <west@paperequator.com>
2018-12-12 16:57:34 -0500

    Database Checks & Inspection
    - analyzes database to make sure top-level fields are present
    - automatically destroys data at the top-level that we do not expect / want
    - initializes database if it does not already exist
    - performs basic check on data each time browser page loads (this can be optimized in the future)
    - add methods to print a single node or an entire tree of nodes

--- makefile
+++ makefile
@@ -8,9 +8,9 @@ TAG?=latest
 
 pack:
 	browserify platform/helpers/array.js \
+		platform/helpers/string.js \
 		platform/helpers/math.js \
 		platform/config/fetch.js \
-		platform/config/db.js \
 		platform/vendor/core.js \
 		platform/vendor/storage.js \
 		platform/modules/network/database.js \
--- platform/modules/network/database.js
+++ platform/modules/network/database.js
@@ -2,34 +2,107 @@
 
 const LX = window.LX || {}; if (!window.LX) window.LX = LX;
 
-LX.Database = class Database extends LV.EventEmitter {
+LX.SharedDatabase = class SharedDatabase extends LV.EventEmitter {
 
-    constructor() {
+    constructor(uri) {
         super();
 
-        this.stor = LV.GraphDB(document.baseURI + "gun");
-        this.root = this.stor.get(LC.db.namespace);
+        this.namespace = "__LX__";
+        this.root = null; // root node
+        this.template = {
+            "pkg": {},
+            "org": {}
+        } // template for creating a root node
+        this.stor = LV.GraphDB(uri); // database instance
+        this.packages = {}; // installed packages
+        this.objects = {}; // cached shared objects
 
-        this.packages = {}; // keep track of a subset of overall database
+        this._keys_omit = ["#", "_"] // ignore these when traversing database results
 
-        this.objects = {};
-
-        this.root.once((v,k) => {
-            if (v == undefined) {
-                let obj = {
-                    "marker": {}
-                }
-                this.stor.get(LC.db.namespace).put(obj).once((v,k) => {
-                    //console.log("[DB] Created root node:", k, v)
-                });
-            }
-            else {
-                //console.log("[DB] Existing root node:", k, v)
-            }
-        });
-
+        this.check(this.namespace)
+            .catch((err) => {
+                console.error(err);
+                this.initialize(this.namespace, {force: true});
+            });
     }
 
+    /**
+    * Check for integrity of database to make sure we have expected data
+    */
+    check(namespace) {  
+        let self = this;
+        return new Promise((resolve, reject) => {
+            // do we have good structure in the database?
+            let top_level_node = self.stor.get(namespace);
+            top_level_node
+                .once(function(v,k) {
+                    let invalid_keys = [];
+
+                    if (typeof(v) != "object") {
+                         return reject(`[Database] Check failed. Missing database namespace: ${namespace}`);
+                    }
+
+                    // compare to our template for the node to make sure nodes are expected
+                    Object.keys(v).forEach(key => {
+                        if (key != "_" && !self.template.hasOwnProperty(key)) {
+                            if ( v[key] === null) {
+                                // already nullified
+                                return;
+                            }
+                            // nullify bad value
+                            top_level_node.get(key).put(null);
+                            console.log("[Database] Nullify bad value for key: " + key);
+                            invalid_keys.push(key);
+                        }
+                        else if (self.template.hasOwnProperty(key) && v[key] === null) {
+                            console.log(`[Database] Value ${key} is null but should not be`);
+                            top_level_node.get(key).put(self.template[key]);
+                        }
+                    });
+                            
+                    if (invalid_keys.length > 0) {
+                        reject(`[Database] Cleared unexpected keys at root of node ${namespace}: ${invalid_keys.join(", ")}`);
+                    }
+                    // expected all nodes. good to go!
+                    self.root = top_level_node;
+                    console.log("[Database] Check completed successfully for namespace: " + namespace);
+                    self.emit("check");
+                    resolve();
+                });
+        });
+    }
+
+    /**
+    * Create entirely new database at our selected root
+    */
+    initialize(namespace, opts) {
+        console.log(`[Database] Initializing at namespace: ${namespace}`);
+        let self = this;
+        opts = opts || {};
+        return new Promise((resolve, reject) => {
+            let root = self.stor.get(namespace);
+            root.once(function(v,k) {
+                // caution: forced initialization is possible here but will reset entire database
+                if (v == undefined || opts.force) {
+                    self.stor.get(namespace)
+                        .put(self.template)
+                        .once((v,k) => {
+                            console.log(`[Database] Created root node: ${namespace}`);
+                            self.emit("initialize");
+                            resolve(namespace);
+                        });
+                }
+                else {
+                    console.log(`[Database] Existing root node found: ${namespace}`);                       
+                    resolve(namespace);
+                }
+
+            });
+        });
+    }
+
+
+    //-------------------------------------------------------------------------
     /**
     * Get node from within root namespace
     */
@@ -45,14 +118,71 @@ LX.Database = class Database extends LV.EventEmitter {
     }
 
 
-    /**
-    * Keeps track of shared objects
-    */
 
-    link(shared_obj) {
-        this.objects[shared_obj.id] = shared_obj;
+    //-------------------------------------------------------------------------
+   
+    /**
+    * Prints out value of a node selected by a path/to/node
+    */
+    print(path,pointer,node) {
+        // recursive attempt to narrow down to target node
+        if (!pointer) pointer = path;
+        if (!node) node = this.root;
+        let split = pointer.split("/");
+        node.get(split[0]).once((v,k) => {
+            if (split.length > 1) {
+                let new_pointer = split.slice(1).join("/");
+                node = node.get(k);
+                this.print(path,new_pointer,node);
+            }
+            else {
+                // we reached the target node here
+                console.log(`[Database] ${path} = `, v);
+            }
+        });
+        return split.length;
     }
 
+    /**
+    *  Print out the graph structure of a specified node
+    */
+    inspect(node,level) {
+        level = level || "";
+        node = node || this.stor.get(this.namespace);
+        node.map().once((v,k) => {
+            
+            if (this._keys_omit.indexOf(k) != -1) {
+                return;
+            }
+
+            if (v === null) {
+                console.log(`${level}[ø] ${k.truncate(30)}`);
+            }
+            else if (typeof(v) == "object") {
+                console.log(`${level}[+] ${k.truncate(30)}`);
+                this.inspect(node.get(k),level+"  ");
+            }
+            else {
+                console.log(`${level}|- ${k.truncate(30)} = `, v.truncate(30));
+            }
+        });
+        return "===== Inspect Database =====";
+    }
+
+
+
+
+    //-------------------------------------------------------------------------
+    /**
+    * Makes it easy to find object to match database node
+    */
+    link(shared_obj) {
+        this.objects[shared_obj.id] = shared_obj;
+    } 
+
+    /**
+    * Drop the reference to the object to match database node
+    */
     unlink(shared_obj) {
         this.objects[shared_obj.id] = null;
     }
