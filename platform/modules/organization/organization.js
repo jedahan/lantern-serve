629964d69f18ba002a8fef38c848e937865e249b
West Lane <west@paperequator.com>
2018-12-14 10:34:53 -0500

    Promisify Organization Mehods

--- platform/modules/organization/organization.js
+++ platform/modules/organization/organization.js
@@ -42,33 +42,51 @@ LX.Organization = class Organization extends LV.EventEmitter {
     * Publish a new data package to the network
     */
     register(name) {
-        if (!name) {
-            return console.error(`${this.log_prefix} please name your organization to register`);
-        }
-    
-        this.org_node.get(name).once((v,k) => {
-            if (v !== null) {
-                return console.log(`${this.log_prefix} organization already exists with name ${name}`);
-                return;
+        return new Promise((resolve, reject) => {
+            if (!name) {
+                console.error(`${this.log_prefix} please name your organization to register`);
+                return reject("name_required");
             }
+        
+            this.org_node.once((v,k) => {
+                if (v) {
+                    console.warn(`${this.log_prefix} organization already exists`,v);
+                    return resolve(v);
+                }
 
-            this.org_node.put({
-                    "name": name,
-                    "members": {},
-                    "packages": {},
-                })
-                .once(() => {
-                    this.emit("register");
-                })
+                this.org_node.put({
+                        "name": name,
+                        "members": {},
+                        "packages": {}
+                    }, (ack) => {
+                        if (ack.err) {
+                            reject(ack.err);
+                        }
+                        else {
+                            this.emit("register");
+                            return resolve();
+                        }
+                    });
+            });
+
         });
 
     }
 
-    unregister() {
-        this.org_node.put(null)
-            .once((v,k) => {
-                console.log(`${this.log_prefix} unregistered`)
-                this.emit("unregister");
+    unregister(name) {
+        return new Promise((resolve, reject) => {
+
+            if (!name) {
+               console.error(`${this.log_prefix} please name your organization to register`);
+               return reject("name_required");
+            }
+        
+            this.org_node.put(null)
+                .once((v,k) => {
+                    console.log(`${this.log_prefix} unregistered ${name}`)
+                    this.emit("unregister");
+                    return resolve();
+                });
             });
     }
 
@@ -79,16 +97,22 @@ LX.Organization = class Organization extends LV.EventEmitter {
     * Add member user to the organization
     */
     addOneMember(user) {
-        this.org_node.get("members")
-            .set(user);
+        return new Promise((resolve, reject) => {
+            this.org_node.get("members")
+                .set(user)
+                .once(resolve);
+            });
     }
 
     /**
     * Remove member user from the organization
     */
     removeOneMember(user) {
-        this.org_node.get("members")
-            .unset(user);
+        return new Promise((resolve, reject) => {
+            this.org_node.get("members")
+                .unset(user)
+                .once(resolve);
+            });
     }
 
 
@@ -99,54 +123,70 @@ LX.Organization = class Organization extends LV.EventEmitter {
     */
     publish(name, objects, version, is_public) {
 
-        if (!name) {
-            return console.error(`${this.log_prefix} please name your package to publish`);
-        }
+        return new Promise((resolve, reject) => {
 
 
-        const publishVersion = () => {
-            this.db.get("pkg").get(name).get("data").get(version)
-                .put(objects)
-                .once((v,k) => {
-                    console.log(`${this.log_prefix} published ${name} version ${version}`);
-                    this.emit("publish", name);
-                });
-        }
-
-
-        let data = {};
-        version = version || "0.0.1";
-        objects = objects || {};
-
-        let publish_package = {
-            "name": name,
-            "version": version,
-            "public": (is_public === true || is_public === null ? true : false)
-        };
-
-        let pkg_node = this.db.get("pkg").get(name);
-
-        pkg_node.once((v,k) => {
-            if (!v) {
-                pkg_node.put(publish_package);
-
-                this.db.get("pkg").get(name)
-                    .get("organization")
-                    .put(this.org_node);
-                publishVersion();
+            if (!name) {
+                console.error(`${this.log_prefix} please name your package to publish`);
+                return reject("name_required");
             }
-            else {
-                pkg_node.get("data").get(version)
-                    .once((v,k) => {
-                        // publishing a version that does not yet exist
-                        if (!v) {
-                            publishVersion();
+
+            let pkg_node = this.db.get("pkg").get(name);
+
+            const publishVersion = () => {
+                this.db.get("pkg").get(name).get("data").get(version)
+                    .put(objects, (ack) => {
+                        if (ack.err) {
+                            reject(ack.err);
                         }
                         else {
-                            console.log(`${this.log_prefix} version ${version} for ${name} already exists`);
+                            console.log(`${this.log_prefix} published ${name} version ${version}`);
+                            this.emit("publish", name);
+                            resolve(pkg_node);
                         }
                     });
             }
+
+
+            let data = {};
+            version = version || "0.0.1";
+            objects = objects || {};
+
+            let publish_package = {
+                "name": name,
+                "version": version,
+                "public": (is_public === true || is_public === null ? true : false)
+            };
+
+            console.log("package ot publish is", publish_package);
+
+            pkg_node.once((v,k) => {
+                if (!v) {
+                    pkg_node.put(publish_package).once(() => {
+
+                        // package node created
+                        this.org_node.get("packages").get(name).put(pkg_node);
+
+                        this.db.get("pkg").get(name)
+                            .get("organization")
+                            .put(this.org_node, publishVersion)
+                    });
+                }
+                else {
+                    pkg_node.get("data").get(version)
+                        .once((v,k) => {
+                            // publishing a version that does not yet exist
+                            if (!v) {
+                                publishVersion();
+                            }
+                            else {
+                                console.log(`${this.log_prefix} version ${version} for ${name} already exists`);
+                                resolve(pkg_node)
+                            }
+                        });
+                }
+            });
+
         });
     }
 
@@ -156,29 +196,33 @@ LX.Organization = class Organization extends LV.EventEmitter {
     * Unpublish removes a data package from the network
     */
     unpublish(name, version) {
-        
-        if (!name) {
-            return console.error(`${this.log_prefix}please input name of package to unpublish (this is a destructive action)`);
-        }
+        return new Promise((resolve, reject) => {
 
-        const cb = (v,k) => {
-            this.emit("unpublish", name);
-        }
+            if (!name) {
+                console.error(`${this.log_prefix}please input name of package to unpublish (this is a destructive action)`);
+                return reject("name_required");
+            }
 
-        let node = this.db.get("pkg").get(name);
+            const cb = (v,k) => {
+                this.emit("unpublish", name);
+                return resolve();
+            }
 
-        if (!version) {
-            // unpublish all versions
-            node.put(null).once(cb);
-        }
-        else {
-            node.get("version").once((v,k) => {
-                if (v == version) {
-                    node.get("version").put(null);
-                }
-                node.get("data").get(version).put(null).once(cb);
-            });
-        }
+            let node = this.db.get("pkg").get(name);
+
+            if (!version) {
+                // unpublish all versions
+                node.put(null, cb);
+            }
+            else {
+                node.get("version").once((v,k) => {
+                    if (v == version) {
+                        node.get("version").put(null);
+                    }
+                    node.get("data").get(version).put(null,cb);
+                });
+            } 
+        });
     }
 
 }
