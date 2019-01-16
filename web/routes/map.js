283f47cfa8145dd06d8780476005bf5ff9d3922b
West Lane <west@paperequator.com>
2019-01-15 22:25:23 -0500

    Map Tile Optimizations
    - faster load times
    - auto-repair badly loaded tiles
    
    
    Former-commit-id: 4e4e2a5d7286d718e193a0b6036cc96c492836c1

--- web/routes/map.js
+++ web/routes/map.js
@@ -24,7 +24,6 @@ module.exports = (serv) => {
 	const getLocalPathForTile = (params) => {
 		let zxy = `${params.z}/${params.x}/${params.y}.png`;
 		let target_dir = tiles_dir + "/"+path.dirname(zxy);
-		fs.ensureDirSync(target_dir);
 		let file_name = path.basename(zxy);
 		let file_path = target_dir + "/" + file_name;
 		return file_path;
@@ -43,6 +42,39 @@ module.exports = (serv) => {
 		});
 	}
 
+	const getTileFromCloud = (req, res) => {
+
+		let url = "https://maps.tilehosting.com" + req.url;
+		//log.debug("Map tile proxy target is:", url);
+		let do_cache = false;
+
+		return fetch(url, {
+				cors: true,
+				headers: {
+					"Origin": util.getDomain()
+				}
+			}, 1000, "Unable to access map tile in time")
+			.then((body) => {
+				if (body.status == 200) {
+					do_cache = true;
+					return body.buffer();
+				}
+				else {
+					//log.warn(`Map tile request failed: ${body.statusText} (${body.status})`);
+					throw new Error("Map tile request failed");
+				}
+			})
+			.then((buffer) => {
+				res.type("png");
+				res.send(buffer);
+				return do_cache;
+			})
+			.catch((e) => {
+				//log.warn(`Map tile request failed: ${url}`);
+				return sendEmptyTile(res);
+			});
+	}
+ 
 
 
 	//---------------------------------------------------------------------- 
@@ -50,62 +82,44 @@ module.exports = (serv) => {
 	* MapTiler Proxy
 	*/
 	serv.get("/c/:id/styles/:map/:z/:x/:y.png", (req, res, next) => {
-			
-		let local_path = getLocalPathForTile(req.params);
-
 		// use offline cache if available, avoids hitting external sever
-		if (fs.existsSync(local_path)) {
-			// log.debug("use cached tile", local_path);
-			fs.readFile(local_path, (err, buffer) => {
-				res.type("png");
-				res.send(buffer)
-			});
-			return;
-		}
+		let local_path = getLocalPathForTile(req.params);
+	  
+		//log.debug("use cached tile", local_path);
 
-		let url = "https://maps.tilehosting.com" + req.url;
+		fs.readFile(local_path, (err, buffer) => {
 
-
-		if (!assume_internet) {
-			// log.debug(`Skip offline attempt for: ${url}`);
-			return sendEmptyTile(res);
-		}
-
-
-		//log.debug("Map tile proxy target is:", url);
-		let do_cache = false;
-		fetch(url, {
-			cors: true,
-			headers: {
-				"Origin": util.getDomain()
+			if (err && err.code == "ENOENT" || buffer.length < 100) {
+				if (!assume_internet) {
+					//log.debug(`Skip offline attempt for: ${url}`);
+					return sendEmptyTile(res);
+				}
+				else {
+					getTileFromCloud(req,res)
+					.then((do_cache) => {
+						if (do_cache) {
+							// also save to cache
+							// @todo this could be turned into a proper queue in the future
+							let delay = 1000 + (2000*Math.random());
+							
+							setTimeout(() => {
+								// use timeout to help prioritize immediate network requests over saving to disk
+								//log.debug(`Cache tile: ${req.url}`);
+								fs.ensureDirSync(path.dirname(local_path));
+								fs.writeFile(local_path, buffer);
+							}, delay);
+						}
+					});
+				}
 			}
-		}, 1000, "Unable to access map tile in time").then((body) => {
-			if (body.status == 200) {
-				do_cache = true;
-				return body.buffer();
+			else if (err) {
+				log.error(err);
 			}
 			else {
-				log.warn(`Map tile request failed: ${body.statusText} (${body.status})`);
-				throw new Error("Map tile request failed");
+				res.type("png");
+				res.send(buffer);
 			}
-		})
-		.then((buffer) => {
-			res.type("png");
-			res.send(buffer);
-			if (do_cache) {
-				// also save to cache
-				// @todo this could be turned into a proper queue in the future
-				let delay = 500+7000*Math.random();
-				setTimeout(() => {
-					// use timeout to help prioritize immediate network requests over saving to disk
-			    	//log.debug(`Cache tile: ${req.url}`);
-					fs.writeFile(local_path, buffer);
-				}, delay)
-		    }
-		})
-		.catch((e) => {
-			log.warn(`Map tile request failed: ${url}`);
-			return sendEmptyTile(res);
 		});
+
 	});
 }
