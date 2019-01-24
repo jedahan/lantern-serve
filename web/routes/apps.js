407876723dde79092180da33157dd3f75dec2940
West Lane <west@paperequator.com>
2019-01-24 17:37:14 -0500

    App Update Also Re-compiles Platform Bundles

--- web/routes/apps.js
+++ web/routes/apps.js
@@ -64,5 +64,13 @@ module.exports = (serv) => {
                 log.info('git pull for apps: ', stdout)
             }
         })
+
+        // also re-generate scripts so we're always working with the most up-to-date
+        util.packJavascript()
+        .then(util.compressStylesheets)
+        .then(util.compressJavascript)
+        .then(() => {
+            log.info("javascript and stylesheets recompiled...")
+        })
     })
 }
